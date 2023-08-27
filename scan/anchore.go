466e3803f6256ce7e066b871c1756c85c17992b5
c-lekha <c_lprabhakaran@cloudbees.com>
2023-08-27 19:13:18 +0530

    feat(CBCE-6730): Initial draft for getting analysis and vulns

---
+++ scan/anchore.go
@@ -0,0 +1,128 @@
+package scan
+
+import (
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/cloudbees-compliance/chlog-go/log"
+)
+
+type AnchoreScanInterface interface {
+	GetImage(requestId string, imageName string) ([]byte, error)
+	GetVulnerabilities(requestId string, imageName string) ([]byte, error)
+	GetRegistries(requestId string) ([]byte, error)
+}
+
+type AnchoreWrapper struct {
+}
+
+// create a package level variable of type interface
+var IAnchoreInterface AnchoreScanInterface
+
+func init() {
+	IAnchoreInterface = AnchoreWrapper{}
+}
+
+func GetImage(requestId string, imageName string) ([]byte, error) {
+	log.Debug(requestId).Msgf("Getting scanned image...")
+
+	out, err := IAnchoreInterface.GetImage(requestId, imageName)
+
+	if err != nil {
+		// only output stdout/err if there was a problem
+		if out != nil {
+			log.Error(requestId).Msg("stdout/err:" + string(out))
+		}
+		return nil, err
+	}
+	return out, nil
+}
+
+func GetScanStatus(requestId string, imageName string) (*GetAnalysisStatus, bool, error) {
+	status, err := GetImage(requestId, imageName)
+	if err != nil {
+		return nil, false, err
+	}
+	var analysisStatus GetAnalysisStatus
+	jsonerr := json.Unmarshal(status, &analysisStatus)
+	if jsonerr != nil {
+		log.Error().Msgf("AnchorePlugin: Error when marshaling response %s - %s", analysisStatus.AnalysisStatus, analysisStatus.ImageStatus)
+		return nil, false, jsonerr
+	}
+	log.Debug().Msgf("AnchorePlugin: Get image status  %s - %s", analysisStatus.AnalysisStatus, analysisStatus.ImageStatus)
+	if strings.Compare("active", analysisStatus.ImageStatus) == 0 &&
+		strings.Compare("analyzed", analysisStatus.AnalysisStatus) == 0 {
+		return &analysisStatus, true, nil
+	} else if strings.Compare("active", analysisStatus.ImageStatus) == 0 &&
+		strings.Compare("analyzed", analysisStatus.AnalysisStatus) != 0 {
+		var attempts = RetryCount
+		sleep := time.Second * SleepDuration
+		log.Debug(requestId).Msgf("Starting Retry for %d times ...", attempts)
+		for i := 0; i < attempts; i++ {
+			status, err = GetImage(requestId, imageName)
+			if err != nil {
+				return nil, false, err
+			}
+			jsonerr := json.Unmarshal(status, &analysisStatus)
+			if jsonerr != nil {
+				log.Error().Msgf("AnchorePlugin: Error when marshaling response %s - %s", analysisStatus.AnalysisStatus, analysisStatus.ImageStatus)
+				return nil, false, jsonerr
+			}
+			if strings.Compare("active", analysisStatus.ImageStatus) == 0 &&
+				strings.Compare("analyzed", analysisStatus.AnalysisStatus) != 0 {
+				log.Debug(requestId).Msgf("status of analysis for attempt %d is - %s", i+1, analysisStatus.AnalysisStatus)
+				log.Debug(requestId).Msgf("sleeping for : %s ", sleep.String())
+				time.Sleep(sleep)
+				continue
+			}
+			break
+		}
+
+	} else {
+		return nil, false, nil
+	}
+	return &analysisStatus, true, nil
+}
+
+func GetVulnerabilities(requestId string, imageName string) (*[]VulnerabilityDetail, error) {
+	var vulnerabilityList []VulnerabilityDetail
+	vulnerabilities, err := IAnchoreInterface.GetVulnerabilities(requestId, imageName)
+	if err != nil {
+		// only output stdout/err if there was a problem
+		if vulnerabilities != nil {
+			log.Error(requestId).Msg("stdout/err:" + string(vulnerabilities))
+		}
+		return nil, err
+	}
+
+	jsonerr := json.Unmarshal(vulnerabilities, &vulnerabilityList)
+	if jsonerr != nil {
+		log.Error().Msgf("AnchorePlugin: Error when marshaling response for vulnerabilities")
+		return nil, jsonerr
+	}
+
+	return &vulnerabilityList, nil
+
+}
+
+func GetRegistries(requestId string) (*[]Registry, error) {
+	var registryList []Registry
+	registries, err := IAnchoreInterface.GetRegistries(requestId)
+	if err != nil {
+		// only output stdout/err if there was a problem
+		if registries != nil {
+			log.Error(requestId).Msg("stdout/err:" + string(registries))
+		}
+		return nil, err
+	}
+
+	jsonerr := json.Unmarshal(registries, &registryList)
+	if jsonerr != nil {
+		log.Error().Msgf("AnchorePlugin: Error when marshaling response for registries")
+		return nil, jsonerr
+	}
+
+	return &registryList, nil
+
+}
--- scan/definitions.go
+++ scan/definitions.go
@@ -10,3 +10,23 @@ const HttpProtocol = "http://"
 const CreateTokenEndPoint = "/api/v1/login"
 const PostMethodType = "POST"
 const ContentType = "application/json"
+const RetryCount = 8
+const SleepDuration = 30
+const DockerRepo = "dockerhub_repo"
+const JfrogRepo = "jfrog_server"
+const NexusRepo = "nexus_repo_binary"
+
+type GetAnalysisStatus struct {
+	AnalysisStatus string `json:"analysisStatus,omitempty"`
+	ImageStatus    string `json:"imageStatus,omitempty"`
+}
+
+type VulnerabilityDetail struct {
+	CveId   string `json:"vulns,omitempty"`
+	Url     string `json:"url,omitempty"`
+	Package string `json:"package,omitempty"`
+}
+
+type Registry struct {
+	RegistryName string `json:"registryName,omitempty"`
+}
