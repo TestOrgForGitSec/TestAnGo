3f6ee8e4668e82a5538ad9926777bdd640823550
c-lekha <c_lprabhakaran@cloudbees.com>
2023-09-13 17:52:36 +0530

    feat(CBCE-7154): Added unit test cases

--- service_test.go
+++ service_test.go
@@ -11,6 +11,7 @@ import (
 	service "github.com/cloudbees-compliance/chplugin-go/v0.4.0/servicev0_4_0"
 	plugin "github.com/cloudbees-compliance/chplugin-service-go/plugin"
 	scan "github.com/cloudbees-compliance/compliance-hub-plugin-anchore/scan"
+	"github.com/cloudbees-compliance/compliance-hub-plugin-anchore/testdata"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/proto"
 )
@@ -27,8 +28,39 @@ func (f *PluginFetcher) FetchAssets(req plugin.AssetFetchRequest) ([]*domain.Ass
 
 	assetProfile.BinAttributes = append(assetProfile.BinAttributes, binaryAttribute)
 	assetProfiles = append(assetProfiles, assetProfile)
-	asset := &domain.Asset{Uuid: "1", MasterAsset: &domain.MasterAsset{Type: "BINARY", SubType: "subtype", Identifier: "localhost"}, Profiles: assetProfiles}
+	asset := &domain.Asset{Uuid: "1", MasterAsset: &domain.MasterAsset{Type: "BINARY", SubType: "aws_ecr_repo", Identifier: "aws_ecr_repoarn:aws:ecr:us-east-1:1234567:repository/test/plugin-test"}, Profiles: assetProfiles}
 	assets = append(assets, asset)
+
+	//asset 2
+	var assetProfiles2 []*domain.AssetProfile
+	binaryAttribute2 := &domain.BinaryAttribute{Name: "imageName", Type: "PRIMARY", Version: "1.0", SourceType: domain.SourceType_STREAM, Data: []byte("{sampledata}")}
+	assetProfile2 := &domain.AssetProfile{Uuid: "testProfileuuid", Identifier: "v1.0.1", Type: "BINARY", AttributesUuid: "testattriuuid"}
+
+	assetProfile2.BinAttributes = append(assetProfile2.BinAttributes, binaryAttribute2)
+	assetProfiles2 = append(assetProfiles2, assetProfile2)
+	asset2 := &domain.Asset{Uuid: "1", MasterAsset: &domain.MasterAsset{Type: "BINARY", SubType: "dockerhub_repo", Identifier: "library/test"}, Profiles: assetProfiles2}
+	assets = append(assets, asset2)
+
+	//asset 3
+	var assetProfiles3 []*domain.AssetProfile
+	binaryAttribute3 := &domain.BinaryAttribute{Name: "imageName", Type: "PRIMARY", Version: "1.0", SourceType: domain.SourceType_STREAM, Data: []byte("{sampledata}")}
+	assetProfile3 := &domain.AssetProfile{Uuid: "testProfileuuid", Identifier: "v1.0.1", Type: "BINARY", AttributesUuid: "testattriuuid"}
+
+	assetProfile3.BinAttributes = append(assetProfile3.BinAttributes, binaryAttribute3)
+	assetProfiles3 = append(assetProfiles3, assetProfile3)
+	asset3 := &domain.Asset{Uuid: "1", MasterAsset: &domain.MasterAsset{Type: "BINARY", SubType: "artifactory_repo", Identifier: "https://jfrog.test.com/artifactory/test/plugin"}, Profiles: assetProfiles3}
+	assets = append(assets, asset3)
+
+	//asset 4
+	var assetProfiles4 []*domain.AssetProfile
+	binaryAttribute4 := &domain.BinaryAttribute{Name: "imageName", Type: "PRIMARY", Version: "1.0", SourceType: domain.SourceType_STREAM, Data: []byte("{sampledata}")}
+	assetProfile4 := &domain.AssetProfile{Uuid: "testProfileuuid", Identifier: "v1.0.1", Type: "BINARY", AttributesUuid: "testattriuuid"}
+
+	assetProfile4.BinAttributes = append(assetProfile4.BinAttributes, binaryAttribute4)
+	assetProfiles4 = append(assetProfiles4, assetProfile4)
+	asset4 := &domain.Asset{Uuid: "1", MasterAsset: &domain.MasterAsset{Type: "BINARY", SubType: "nexus_repo_binary", Identifier: "https://nexus.com/#browse/browse:docker-hosted-repo:v2/test"}, Profiles: assetProfiles4}
+	assets = append(assets, asset4)
+
 	return assets, nil
 }
 
@@ -56,11 +88,11 @@ func TestGetAssetDescriptors(t *testing.T) {
 	log.Debug().Msg("Inside TestGetAssetDescriptors - Exit")
 }
 
-func mockExecuteRequest() *service.ExecuteRequest {
+func mockEcrExecuteRequest() *service.ExecuteRequest {
 	acct := &domain.Account{Uuid: "12245"}
 	str := []string{"aws_ecr_repo"}
 	metadataStr := []byte(`{"url":"testurl","userName":"test","password":"test","accountName":"test"}`)
-	assetIds := []string{"localhost"}
+	assetIds := []string{"arn:aws:ecr:us-east-1:1234567:repository/test/plugin-test"}
 	profileIds := []string{""}
 	req := &service.ExecuteRequest{Account: acct, AssetType: "BINARY", AssetSubTypes: str, Metadata: metadataStr,
 		AssetIdentifiers: assetIds, ProfileIdentifiers: profileIds}
@@ -71,7 +103,7 @@ func mockExecuteRequestErr() *service.ExecuteRequest {
 	acct := &domain.Account{Uuid: "12245"}
 	str := []string{"aws_ecr_repo"}
 	metadataStr := []byte(`{"url""testurl","username":"test","password":"test","accountName":"test"}`)
-	assetIds := []string{"localhost"}
+	assetIds := []string{"arn:aws:ecr:us-east-1:1234567:repository/test/plugin-test"}
 	profileIds := []string{""}
 	req := &service.ExecuteRequest{Account: acct, AssetType: "BINARY", AssetSubTypes: str, Metadata: metadataStr,
 		AssetIdentifiers: assetIds, ProfileIdentifiers: profileIds}
@@ -110,4 +142,24 @@ func TestGetNetListener(t *testing.T) {
 	getGrpcServer(1024*1024*1024, 3, 5)
 	InitConfig()
 	log.Debug().Msg("TestGetNetListener - Exit")
+}
+
+func TestExecuteAnalysersuccessecrrepo(t *testing.T) {
+	log.Debug().Msg("TestExecuteAnalysersuccess - Enter")
+	anchore := NewAnchoreScanner()
+	req := mockEcrExecuteRequest()
+	fetcher := &PluginFetcher{}
+
+	testdata.MockGetSystemStatus("testdata/getimage.json")
+	testdata.MockGetRegistries("testdata/getregistries.json")
+	testdata.MockGetImage("testdata/getimage.json")
+	testdata.MockGetVulnerabilities("testdata/getVulnerabilities.json")
+	mockVar := testdata.HttpMock1{}
+
+	scan.IAnchore = mockVar
+	res, err := anchore.ExecuteAnalyser(context.Background(), req, fetcher, nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	log.Debug().Msg("TestExecuteAnalysersuccess - Exit")
+
 }
